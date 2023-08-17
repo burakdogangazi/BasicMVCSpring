bc0aba8f03b0cd37d6ba1ca989cd0758cce9ae26
“Burak <ddoganbburak@gmail.com>
2023-08-18 00:16:30 +0300

    initial commit

---
+++ src/test/java/bd/springframework/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.springframework.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
